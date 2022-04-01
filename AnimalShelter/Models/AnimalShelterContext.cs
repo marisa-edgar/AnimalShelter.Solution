7f1b66e45fda0537c93ac6a559e725075a87122e
marisa-edgar <marisaedgar1212@gmail.com>
2022-04-01 12:27:59 -0700

    add another animal, break for lunch

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -17,7 +17,8 @@ namespace AnimalShelter.Models
                   new Animal { AnimalId = 2, Name = "Pops", Species = "Dog", Breed = "Pit Bull", Age = 1, Gender = "Female" },
                   new Animal { AnimalId = 3, Name = "Trash Boat", Species = "Dog", Breed = "Shiba Inu", Age = 2, Gender = "Female" },
                   new Animal { AnimalId = 4, Name = "Rigby", Species = "Cat", Breed = "Spinx", Age = 4, Gender = "Male" },
-                  new Animal { AnimalId = 5, Name = "Mordechai", Species = "Dog", Breed = "Greyhound", Age = 3, Gender = "Male" }
+                  new Animal { AnimalId = 5, Name = "Mordechai", Species = "Dog", Breed = "Greyhound", Age = 3, Gender = "Male" },
+                  new Animal { AnimalId = 6, Name = "Skips", Species = "Cat", Breed = "Manx", Age = 6, Gender = "Male" }
               );
         }
 
