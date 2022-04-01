e1df2890d05775ef121022917f817e84404f2f53
marisa-edgar <marisaedgar1212@gmail.com>
2022-04-01 09:27:11 -0700

    add file structure and models

---
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Models
+{
+    public class AnimalShelterContext : DbContext
+    {
+        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
+
