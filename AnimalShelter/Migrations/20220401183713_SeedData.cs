3944947dc456c90519d2d12a6d4e2b9780ae7bca
marisa-edgar <marisaedgar1212@gmail.com>
2022-04-01 11:37:48 -0700

    seed data

---
+++ AnimalShelter/Migrations/20220401183713_SeedData.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Breed",
+                table: "Animals",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Breed", "Gender", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 7, "Lab", "Female", "Benson", "Dog" },
+                    { 2, 1, "Pit Bull", "Female", "Pops", "Dog" },
+                    { 3, 2, "Shiba Inu", "Female", "Trash Boat", "Dog" },
+                    { 4, 4, "Spinx", "Male", "Rigby", "Cat" },
+                    { 5, 3, "Greyhound", "Male", "Mordechai", "Dog" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+
+            migrationBuilder.DropColumn(
+                name: "Breed",
+                table: "Animals");
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -9,7 +9,18 @@ namespace AnimalShelter.Models
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+          builder.Entity<Animal>()
+              .HasData(
+                  new Animal { AnimalId = 1, Name = "Benson", Species = "Dog", Breed = "Lab", Age = 7, Gender = "Female" },
+                  new Animal { AnimalId = 2, Name = "Pops", Species = "Dog", Breed = "Pit Bull", Age = 1, Gender = "Female" },
+                  new Animal { AnimalId = 3, Name = "Trash Boat", Species = "Dog", Breed = "Shiba Inu", Age = 2, Gender = "Female" },
+                  new Animal { AnimalId = 4, Name = "Rigby", Species = "Cat", Breed = "Spinx", Age = 4, Gender = "Male" },
+                  new Animal { AnimalId = 5, Name = "Mordechai", Species = "Dog", Breed = "Greyhound", Age = 3, Gender = "Male" }
+              );
+        }
+
         public DbSet<Animal> Animals { get; set; }
     }
 }
-
