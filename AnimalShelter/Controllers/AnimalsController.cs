d8dcf1ee5455a25f64dfa58c409eeaafd19c70df
marisa-edgar <marisaedgar1212@gmail.com>
2022-04-01 14:41:48 -0700

    WIP versoning

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -19,14 +19,32 @@ namespace AnimalShelter.Controllers
     {
       _db = db;
     }
-
+    // [EnableCors("AnotherPolicy")]
     // GET: api/Animals
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Animal>>> Get()
+    public async Task<ActionResult<IEnumerable<Animal>>> Get(string species, string gender, string name)
     {
-      return await _db.Animals.ToListAsync();
+      var query = _db.Animals.AsQueryable();
+
+      if (species != null)
+      {
+        query = query.Where(entry => entry.Species == species);
+      }
+
+      if (gender != null)
+      {
+        query = query.Where(entry => entry.Gender == gender);
+      }    
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }      
+
+      return await query.ToListAsync();
     }
 
+    // [EnableCors("Policy1")]
     // GET: api/Animals/5
     [HttpGet("{id}")]
     public async Task<ActionResult<Animal>> GetAnimal(int id)
@@ -101,6 +119,121 @@ namespace AnimalShelter.Controllers
     private bool AnimalExists(int id)
     {
       return _db.Animals.Any(e => e.AnimalId == id);
+    }
+  }
+  [ApiVersion("2.0")]
+  [Route("api/Animal")]
+  [ApiController]
+  public class AnimalsV2Controller : ControllerBase
+  {
+    private readonly AnimalShelterContext _db;
+    public AnimalsV2Controller(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+    //api/Animal
+    public async Task<ActionResult<IEnumerable<Animal>>> Get()
+    {
+      return await _db.Animals.ToListAsync();
+    }
+        [HttpGet]
+    public async Task<ActionResult<IEnumerable<Animal>>> Get(string name, string species, int age, string gender)
+    {
+      var query = _db.Animals.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(e => e.Name == name);
+      }
+      if (species != null)
+      {
+        query = query.Where(e => e.Species == species);
+      }
+      if (age > 0)
+      {
+        query = query.Where(e => e.Age >= age);
+      }
+      if (gender != null)
+      {
+        query = query.Where(e => e.Gender == gender);
+      }
+      
+      return await query.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Animal>> Post(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+    }
+
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+
+      if (animal == null)
+      {
+        return NotFound();
+      }
+      return animal;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal)
+    {
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+      _db.Entry(animal).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimalShelter(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+      if (animal != null)
+      {
+        return NotFound();
+      }
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+      return NoContent();
+    }
+        //// Random AnimalShelter Test
+    [HttpGet("random")]
+    public async Task<ActionResult<Animal>> GetRandom()
+    {
+      Random rnd = new Random();
+      int dbCount = _db.Animals.Count();
+      int id = rnd.Next(1, dbCount);
+      var animal = await _db.Animals.FindAsync(id);
+      return animal;
     }
   }
 }
